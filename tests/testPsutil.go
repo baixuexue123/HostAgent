4f3628694ec68220f4d6242f955ecdbae19fb27a
baixue <pysaoke@gmail.com>
2016-11-13 23:19:43 +0800

    test gopsutil

---
+++ tests/testPsutil.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func testVirtualMemory() {
+	// func SwapMemory() (*SwapMemoryStat, error)
+	v1, _ := mem.VirtualMemory()
+
+	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v1.Total, v1.Free, v1.UsedPercent)
+	fmt.Println(v1)
+
+	// func SwapMemory() (*SwapMemoryStat, error)
+	v2, _ := mem.SwapMemory()
+	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v2.Total, v2.Free, v2.UsedPercent)
+	fmt.Println(v2)
+}
+
+func testCpu() {
+	count, _ := cpu.Counts(true)
+	fmt.Printf("Cpu Counts: %v\n", count)
+
+	info, _ := cpu.Info()
+	fmt.Println(info)
+}
+
+func main() {
+	testVirtualMemory()
+}
