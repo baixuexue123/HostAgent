4d3339b67cd38162378fce722d66475f7918c819
baixue <pysaoke@gmail.com>
2016-11-29 23:24:23 +0800

    test pass cpustat cputimes

--- tests/testPsutil.go
+++ tests/testPsutil.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
+	"time"
 )
 
 func testHostInfo() {
@@ -36,6 +37,52 @@ func testUserStat() {
 	}
 }
 
+func testCpuTimesStat() {
+	timesStat, _ := cpu.Times(true)
+
+	for _, stat := range timesStat {
+		fmt.Println()
+		fmt.Printf("Total : %v\n", stat.Total())
+		fmt.Printf("CPU : %s\n", stat.CPU)
+		fmt.Printf("User : %v\n", stat.User)
+		fmt.Printf("System : %v\n", stat.System)
+		fmt.Printf("Idle : %v\n", stat.Idle)
+		fmt.Printf("Nice : %v\n", stat.Nice)
+		fmt.Printf("Iowait : %v\n", stat.Iowait)
+		fmt.Printf("Irq : %v\n", stat.Irq)
+		fmt.Printf("Softirq : %v\n", stat.Softirq)
+		fmt.Printf("Steal : %v\n", stat.Steal)
+		fmt.Printf("Guest : %v\n", stat.Guest)
+		fmt.Printf("GuestNice : %v\n", stat.GuestNice)
+		fmt.Printf("Stolen : %v\n", stat.Stolen)
+	}
+}
+
+func testCpuInfo() {
+	count, _ := cpu.Counts(true) // logical
+	fmt.Printf("Cpu Counts: %v\n", count)
+
+	percent, _ := cpu.Percent(1*time.Second, true)
+	fmt.Printf("Cpu Percent: %v\n", percent)
+
+	infoStat, _ := cpu.Info()
+	for _, stat := range infoStat {
+		fmt.Println()
+		fmt.Printf("CPU : %v\n", stat.CPU)
+		fmt.Printf("VendorID : %s\n", stat.VendorID)
+		fmt.Printf("Family : %s\n", stat.Family)
+		fmt.Printf("Model : %s\n", stat.Model)
+		fmt.Printf("Stepping : %v\n", stat.Stepping)
+		fmt.Printf("PhysicalID : %s\n", stat.PhysicalID)
+		fmt.Printf("CoreID : %s\n", stat.CoreID)
+		fmt.Printf("Cores : %v\n", stat.Cores)
+		fmt.Printf("ModelName : %s\n", stat.ModelName)
+		fmt.Printf("Mhz : %v\n", stat.Mhz)
+		fmt.Printf("CacheSize : %v\n", stat.CacheSize)
+		fmt.Printf("Flags : %v\n", stat.Flags)
+	}
+}
+
 func testVirtualMemory() {
 	// func SwapMemory() (*SwapMemoryStat, error)
 	v1, _ := mem.VirtualMemory()
@@ -49,14 +96,6 @@ func testVirtualMemory() {
 	fmt.Println(v2)
 }
 
-func testCpu() {
-	count, _ := cpu.Counts(true)
-	fmt.Printf("Cpu Counts: %v\n", count)
-
-	info, _ := cpu.Info()
-	fmt.Println(info)
-}
-
 func testDisk() {
 	d, _ := disk.Usage("/")
 	fmt.Printf("HD : %vGB Free : %vGB Usage : %f%%\n", d.Total/1024^3, d.Free/1024^3, d.UsedPercent)
@@ -67,4 +106,8 @@ func main() {
 	testHostInfo()
 	fmt.Println("*****************************************")
 	testUserStat()
+	fmt.Println("*****************************************")
+	testCpuTimesStat()
+	fmt.Println("*****************************************")
+	testCpuInfo()
 }
