4286be52cc7c16128b5bbeffd4510c4aa1cef364
baixue <pysaoke@gmail.com>
2016-12-25 22:34:01 +0800

    test httprouter

---
+++ tests/testHttpRouter.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+	fmt.Printf("%v", r)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Printf("%v", ps)
+	fmt.Printf("%v", r)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
