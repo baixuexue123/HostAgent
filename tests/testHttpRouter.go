db5e8134a1c0c9571084c1eae16cc62042ecaf79
baixue <pysaoke@gmail.com>
2016-12-26 22:54:06 +0800

    add httprouter params

--- tests/testHttpRouter.go
+++ tests/testHttpRouter.go
@@ -10,19 +10,35 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
-	fmt.Printf("%v", r)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-	fmt.Printf("%v", ps)
-	fmt.Printf("%v", r)
+	fmt.Fprintf(w, "%v\n", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "%s; %s; %s\n", r.Method, r.URL, r.Proto)
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
+	router.GET("/handler", handler)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
