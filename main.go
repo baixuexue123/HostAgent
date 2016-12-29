1dd514641a3db3c2d4dcd27937827013e09d7b53
baixue <pysaoke@gmail.com>
2016-12-29 23:11:33 +0800

    glances api

--- main.go
+++ main.go
@@ -10,25 +10,44 @@ import (
 
 func main() {
 
-	http.HandleFunc("/api/alllist", handler)
-	http.HandleFunc("/api/load", counter)
-	http.HandleFunc("/api/cpu", counter)
-	http.HandleFunc("/api/percpu", counter)
-	http.HandleFunc("/api/processlist", counter)
-	http.HandleFunc("/api/process/pid", counter)
-	http.HandleFunc("/api/network", counter)
-	http.HandleFunc("/api/mem", counter)
-	http.HandleFunc("/api/memswap", counter)
-	http.HandleFunc("/api/system", counter)
-	http.HandleFunc("/api/diskio", counter)
-	http.HandleFunc("/api/processcount", counter)
-	http.HandleFunc("/api/now", counter)
-	http.HandleFunc("/api/uptime", counter)
-	http.HandleFunc("/api/core", counter)
-	http.HandleFunc("/api/fs", counter)
-	http.HandleFunc("/api/monitor", counter)
-	http.HandleFunc("/api/version", counter)
-	http.HandleFunc("/api/all", counter)
+	http.HandleFunc("/api/list/", handler)
+
+	http.HandleFunc("/api/version/", counter)
+
+	http.HandleFunc("/api/help/", counter)
+
+	http.HandleFunc("/api/system/", counter)
+
+	http.HandleFunc("/api/now/", counter)
+	http.HandleFunc("/api/uptime/", counter)
+
+	http.HandleFunc("/api/core/", counter)
+	http.HandleFunc("/api/load/", counter)
+	http.HandleFunc("/api/cpu/", counter)
+	http.HandleFunc("/api/percpu/", counter)
+
+	http.HandleFunc("/api/mem/", counter)
+	http.HandleFunc("/api/mem/used/", counter)
+	http.HandleFunc("/api/memswap/", counter)
+
+	http.HandleFunc("/api/processlist/", counter)
+	http.HandleFunc("/api/processlist/pid/", counter)
+	http.HandleFunc("/api/processlist/pid/:pid", counter)
+	http.HandleFunc("/api/processcount/", counter)
+
+	http.HandleFunc("/api/network/", counter)
+	http.HandleFunc("/api/network/interfaces/", counter)
+	http.HandleFunc("/api/network/interface/:iface", counter)
+
+	http.HandleFunc("/api/hddtemp/", counter)
+
+	http.HandleFunc("/api/diskio/", counter)
+
+	http.HandleFunc("/api/fs/", counter)
+
+	http.HandleFunc("/api/monitor/", counter)
+
+	http.HandleFunc("/api/quicklook/", counter)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
